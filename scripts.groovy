552b5afb13780701f2058169a4bbe3d95b117fc1
Mo <mo@aalillou.be>
2021-12-16 20:57:40 +0100

    update

---
+++ scripts.groovy
@@ -0,0 +1,17 @@
+def buildApp() {
+    echo "building the application..."
+}
+
+def testApp() {
+    echo "building the application..."
+}
+
+def buildImage() {
+    echo "building the docker image..."
+}
+
+def deployApp() {
+    echo 'deploying the application...'
+}
+
+return this
