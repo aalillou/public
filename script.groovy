b8425246dd6a6340414cece63dc38383f1448df8
Mo <mo@aalillou.be>
2021-12-17 18:14:40 +0100

    update

--- script.groovy
+++ script.groovy
@@ -16,12 +16,15 @@ def deployApp() {
   sshagent(credentials: ['SSH_PRIVATE_KEY']) {
       echo "conneting with SSH_PRIVATE_KEY"
 
-      def shellCmd = "bash ./server-cmds.sh ${IMAGE_NAME}"
+
 
       def DEPLOY_USER = "gitlab"
       def DEPLOY_SERVER = "aws.moway.be"
+      def CLIENT = "aalillou"
+      def DEPLOY_DEST = "/datadrive/www/$CLIENT.be/admin/"
+      def shellCmd = "cd $DEPLOY_DEST && docker-compose up -d"
 
-      echo "ssh ${DEPLOY_USER}@${$DEPLOY_SERVER}"
+      echo "ssh ${DEPLOY_USER}@${$DEPLOY_SERVER} '$shellCmd'"
 
       // sh 'ssh -o StrictHostKeyChecking=no -l cloudbees 192.168.1.106 uname -a'
       // ssh $DEPLOY_USER@$DEPLOY_SERVER "cd $DEPLOY_DEST && docker-compose up -d"
